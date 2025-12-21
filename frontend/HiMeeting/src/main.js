55dce8747313f874cff232dd9271507c6c352724
weiki3 <wikii3@outlook.com>
2025-12-21 13:13:23 +0800

    clean codes

--- frontend/HiMeeting/src/main.js
+++
@@ -1,6 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'; // Vue 3 的正确引入方式
-import App from './App.vue';
-import router from './router'; // 引入路由配置
-
-createApp(App).use(router).mount('#app'); // 挂载应用并使用路由
